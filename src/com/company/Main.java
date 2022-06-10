4fdcd679953497592f2949877aadf09b20884d18
aaronvictor <aaron.victor30@gmail.com>
2022-06-09 22:42:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import sun.security.x509.OtherName;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Variables
+        int birthMonth = 0;
+        System.out.println("Enter Your Birth Month a 1-12");
+        if (birthMonth >= 1 )
+
+            System.out.println(" Your birthmonth is: N");
+    else {
+            System.out.println("You entered an incorrect month value");
+
+
+        }
+    }
+}
